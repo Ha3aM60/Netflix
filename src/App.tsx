3142b9f9f78644b98a7094d12770622df3782f8a
Ha3aM60 <nazambotop@gmail.com>
2024-03-04 22:14:22 +0200

    add genre update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { AdminLayout } from './layouts/AdminLayout'
+import { AddGenre } from './pages/Admin/AddGenre'
 
 function App() {
 
@@ -8,7 +9,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/control-panel' element={<AdminLayout></AdminLayout>}></Route>
+        <Route path='/control-panel' element={<AdminLayout></AdminLayout>}>
+          <Route path='category'>
+            <Route path='addGenre' element={<AddGenre></AddGenre>}></Route>
+          </Route>
+        </Route>
       </Routes>
 
     </>
