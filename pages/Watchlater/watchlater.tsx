ce61bfb7ee9a71f165eda8d0bed5ab47dde1e72c
Oleg Kharchenko <okharchenko7@icloud.com>
2024-04-12 13:27:39 -0700

    UPDATED

--- pages/Watchlater/watchlater.tsx
+++ pages/Watchlater/watchlater.tsx
@@ -12,20 +12,54 @@ function watchlater() {
           <div className='checklaterdiv'>
               Переглянути пізніше
           </div>
+
           <div className='firstrowfilms'>
               <div className='filmsdiv'>
                 <p className='actuallname1'>Фільми</p>
               </div>
           </div>
+
           <div className='arrowbutton1'>
               <div className='arrowbtnvector'> </div>
           </div>
+
           <div className='godzillaframe'>
             <img src="./images/godzillaimage.jpg"  className='godzillaimg'/>
           </div>
+
           <div className='secondrowfilms'>
               <div className='showsdivsecond'>
                 <p className='showsword'>Серіали</p>  
+              </div>
+          </div>
+
+
+          <div className='thirdrowfilms'>
+              <div className='thirddivsecond'>
+                <p className='thirdword'>Аніме</p>  
+              </div>
+
+              <div className='arrowbutton1'>
+                <div className='arrowbtnvector'> </div>
+              </div>
+
+              <div className='godzillaframe'>
+                <img src="./images/godzillaimage.jpg"  className='godzillaimg'/>
+              </div>
+          </div>
+
+
+          <div className='fourthrowfilms'>
+              <div className='fourthdivsecond'>
+                <p className='fourthword'>Мультфільми</p>  
+              </div>
+
+              <div className='arrowbutton1'>
+                <div className='arrowbtnvector'> </div>
+              </div>
+
+              <div className='godzillaframe'>
+                <img src="./images/godzillaimage.jpg"  className='godzillaimg'/>
               </div>
           </div>
       </div>
