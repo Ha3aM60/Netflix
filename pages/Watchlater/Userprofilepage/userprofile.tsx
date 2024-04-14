ec6bd251d06527e0ee09e6eff46a8e8a3e556a8e
916Shoota <okharchenko7@icloud.com>
2024-04-14 14:57:30 -0700

    User Profil Done

---
+++ pages/Watchlater/Userprofilepage/userprofile.tsx
@@ -0,0 +1,518 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet-async';
+
+import './userprofile.css'
+
+const Userprofile = (props) => {
+  return (
+    <div className="userprofile-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="userprofile-userprofile">
+        <img
+          src="/external/mdicomedy8027-ks9m.svg"
+          alt="mdicomedy8027"
+          className="userprofile-mdicomedy"
+        />
+        <div className="userprofile-bg">
+          <div className="userprofile-group2">
+            <div className="userprofile-group1"></div>
+          </div>
+        </div>
+        <div className="userprofile-footer">
+          <span className="userprofile-text">
+            <span className="userprofile-text001">
+              Маєте запитання? Зателефонуйте за номером 
+            </span>
+            <span>0800-509-416</span>
+          </span>
+          <span className="userprofile-text003">
+            <span>FlexWatch Україна</span>
+          </span>
+          <button className="userprofile-ga2button">
+            <span className="userprofile-text005">
+              <span>Поширені запитання</span>
+            </span>
+          </button>
+          <div className="userprofile-component15">
+            <span className="userprofile-text007">
+              <span>Довідковий центр</span>
+            </span>
+          </div>
+          <div className="userprofile-component19">
+            <span className="userprofile-text009">
+              <span>Обліковий запис</span>
+            </span>
+          </div>
+          <div className="userprofile-component23">
+            <span className="userprofile-text011">
+              <span>Медіацентр</span>
+            </span>
+          </div>
+          <div className="userprofile-component12">
+            <span className="userprofile-text013">
+              <span>Зв’язки з інвесторами</span>
+            </span>
+          </div>
+          <div className="userprofile-component16">
+            <span className="userprofile-text015">
+              <span>Вакансії</span>
+            </span>
+          </div>
+          <div className="userprofile-component20">
+            <span className="userprofile-text017">
+              <span>Способи перегляду</span>
+            </span>
+          </div>
+          <div className="userprofile-component24">
+            <span className="userprofile-text019">
+              <span>Умови використання</span>
+            </span>
+          </div>
+          <div className="userprofile-component13">
+            <span className="userprofile-text021">
+              <span>Конфіденційність</span>
+            </span>
+          </div>
+          <div className="userprofile-component17">
+            <span className="userprofile-text023">
+              <span>Налаштування файлів cookie</span>
+            </span>
+          </div>
+          <div className="userprofile-component21">
+            <span className="userprofile-text025">
+              <span>Корпоративна інформація</span>
+            </span>
+          </div>
+          <div className="userprofile-component25">
+            <span className="userprofile-text027">
+              <span>Зв’язатися з нами</span>
+            </span>
+          </div>
+          <div className="userprofile-component14">
+            <span className="userprofile-text029">
+              <span>Перевірка швидкості</span>
+            </span>
+          </div>
+          <div className="userprofile-component18">
+            <span className="userprofile-text031">
+              <span>Юридичні повідомлення</span>
+            </span>
+          </div>
+          <div className="userprofile-component22">
+            <span className="userprofile-text033">
+              <span>Тільки на Netflix</span>
+            </span>
+          </div>
+        </div>
+        <img
+          src="/external/line48028-3ec4.svg"
+          alt="Line48028"
+          className="userprofile-line4"
+        />
+        <img
+          src="/external/rectangle38028-snwc-1500w.png"
+          alt="Rectangle38028"
+          className="userprofile-rectangle3"
+        />
+        <div className="userprofile-mostpopular">
+          <div className="userprofile-text035">
+            <span className="userprofile-text036">
+              <span>Обліковий запис</span>
+            </span>
+          </div>
+        </div>
+        <img
+          src="/external/line58027-qhfe.svg"
+          alt="Line58027"
+          className="userprofile-line5"
+        />
+        <div className="userprofile-registrationframe">
+          <div className="userprofile-watchlater">
+            <span className="userprofile-text038">
+              <span>Змінити світлину</span>
+            </span>
+          </div>
+          <img
+            src="/external/ellipse138029-yhl9-300h.png"
+            alt="Ellipse138029"
+            className="userprofile-ellipse13"
+          />
+          <img
+            src="/external/line68029-3zdm.svg"
+            alt="Line68029"
+            className="userprofile-line6"
+          />
+          <div className="userprofile-text040">
+            <span className="userprofile-text041">
+              <span>Анна К.</span>
+            </span>
+          </div>
+          <div className="userprofile-adresa1">
+            <span className="userprofile-text043">
+              <span>
+                Анна
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+          </div>
+          <div className="userprofile-adresa2">
+            <span className="userprofile-text045">
+              <span>annakarttt@gmail.com</span>
+            </span>
+          </div>
+          <span className="userprofile-text047">
+            <span>Дані про куристувача</span>
+          </span>
+          <button className="userprofile-buttonstart">
+            <div className="userprofile-frame4">
+              <span className="userprofile-text049">
+                <span>Зберегти зміни</span>
+              </span>
+            </div>
+          </button>
+          <img
+            src="/external/mdipapereditoutline8149-4dx.svg"
+            alt="mdipapereditoutline8149"
+            className="userprofile-mdipapereditoutline"
+          />
+          <span className="userprofile-text051">
+            <span className="userprofile-text052">
+              Приєднався в
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>січні 2024 р.</span>
+          </span>
+        </div>
+        <div className="userprofile-registrationframe1">
+          <span className="userprofile-text054">
+            <span>Вподобання</span>
+          </span>
+          <span className="userprofile-text056">
+            <span>Переглянути пізніше</span>
+          </span>
+          <div className="userprofile-button">
+            <div className="userprofile-watchlater1">
+              <span className="userprofile-text058">
+                <span>Переглянути список</span>
+              </span>
+            </div>
+            <img
+              src="/external/vector8149-uh1i.svg"
+              alt="Vector8149"
+              className="userprofile-vector"
+            />
+          </div>
+          <div className="userprofile-watchlater2">
+            <span className="userprofile-text060">
+              <span>Переглянути список</span>
+            </span>
+            <img
+              src="/external/vectori814-1et.svg"
+              alt="VectorI814"
+              className="userprofile-vector1"
+            />
+          </div>
+          <div className="userprofile-watchlater3">
+            <span className="userprofile-text062">
+              <span>Налаштування</span>
+            </span>
+          </div>
+          <img
+            src="/external/line68149-k08.svg"
+            alt="Line68149"
+            className="userprofile-line61"
+          />
+        </div>
+        <img
+          src="/external/rivisaline8171-kxf4.svg"
+          alt="rivisaline8171"
+          className="userprofile-rivisaline"
+        />
+        <div className="userprofile-safety">
+          <span className="userprofile-text064">
+            <span>Безпека й конфіденційність</span>
+          </span>
+          <div className="userprofile-infomovie">
+            <span className="userprofile-text066">
+              <span>Контролюйте доступ до цього облікового запису,</span>
+              <br></br>
+              <span>переглядайте, які пристрої нещодавно</span>
+              <br></br>
+              <span>використовувалися, тощо.</span>
+            </span>
+          </div>
+          <div className="userprofile-component251">
+            <span className="userprofile-text072">
+              <span>Керування доступом і пристроями</span>
+            </span>
+          </div>
+          <div className="userprofile-component26">
+            <span className="userprofile-text074">
+              <span>Завантажити особисті дані</span>
+            </span>
+          </div>
+          <div className="userprofile-component27">
+            <span className="userprofile-text076">
+              <span>Вийти на всіх пристроях</span>
+            </span>
+          </div>
+        </div>
+        <div className="userprofile-subscription">
+          <span className="userprofile-text078">
+            <span>Підписка й оплата</span>
+          </span>
+          <div className="userprofile-infomovie1">
+            <span className="userprofile-text080">
+              <span>Електронна пошта:</span>
+            </span>
+            <span className="userprofile-text082">
+              <span>Телефон:</span>
+            </span>
+            <span className="userprofile-text084">
+              <span>Дата наступної оплати:</span>
+            </span>
+          </div>
+          <div className="userprofile-infomovie2">
+            <span className="userprofile-text086">
+              <span>annakarttt@gmail.com</span>
+            </span>
+            <span className="userprofile-text088">
+              <span>+380 95 773 63 80</span>
+            </span>
+          </div>
+          <div className="userprofile-infomovie3">
+            <span className="userprofile-text090">
+              <span>**** **** **** 4783</span>
+            </span>
+          </div>
+          <div className="userprofile-infomovie4">
+            <span className="userprofile-text092">
+              <span>18 квітня 2024</span>
+            </span>
+          </div>
+          <div className="userprofile-component201">
+            <span className="userprofile-text094">
+              <span>Змінити адресу електронної пошти</span>
+            </span>
+          </div>
+          <div className="userprofile-component221">
+            <span className="userprofile-text096">
+              <span>Змінити пароль</span>
+            </span>
+          </div>
+          <div className="userprofile-component241">
+            <span className="userprofile-text098">
+              <span>Скористатись промокодом</span>
+            </span>
+          </div>
+          <div className="userprofile-component211">
+            <span className="userprofile-text100">
+              <span>Змінити номер телефону</span>
+            </span>
+          </div>
+          <div className="userprofile-component231">
+            <span className="userprofile-text102">
+              <span>Налаштування способу оплати</span>
+              <br></br>
+              <span>та платіжні дані</span>
+            </span>
+          </div>
+          <button className="userprofile-buttonstart1">
+            <div className="userprofile-frame41">
+              <span className="userprofile-text106">
+                <span>Скасувати підписку</span>
+              </span>
+            </div>
+          </button>
+        </div>
+        <img
+          src="/external/rectangle78238-yj5f-1500w.png"
+          alt="Rectangle78238"
+          className="userprofile-rectangle7"
+        />
+        <div className="userprofile-plan">
+          <span className="userprofile-text108">
+            <span>План</span>
+          </span>
+          <div className="userprofile-plan1">
+            <div className="userprofile-infomovie5">
+              <span className="userprofile-text110">
+                <span className="userprofile-text111">Ваш тариф</span>
+                <span className="userprofile-text112">:</span>
+                <span className="userprofile-text113">
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <span>Стандартний</span>
+              </span>
+            </div>
+            <button className="userprofile-buttonstart2">
+              <div className="userprofile-frame42">
+                <span className="userprofile-text115">
+                  <span>Змінити план</span>
+                </span>
+              </div>
+            </button>
+            <span className="userprofile-text117">
+              <span>1080p (Full HD)</span>
+            </span>
+            <span className="userprofile-text119">
+              <span>Підтримувані пристрої:</span>
+            </span>
+            <span className="userprofile-text121">
+              <span className="userprofile-text122">
+                Пристрої для завантаження: 2
+              </span>
+            </span>
+            <span className="userprofile-text123">
+              <span className="userprofile-text124">
+                Перегляд
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span>без реклами</span>
+            </span>
+            <span className="userprofile-text126">
+              <span>
+                Телевізор, комп’ютер, мобільний
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>телефон, планшет</span>
+            </span>
+          </div>
+          <div className="userprofile-component28">
+            <span className="userprofile-text130">
+              <span>Змінити план</span>
+            </span>
+          </div>
+          <div className="userprofile-component29">
+            <span className="userprofile-text132">
+              <span>Як скасувати підписку</span>
+            </span>
+          </div>
+          <div className="userprofile-component30">
+            <span className="userprofile-text134">
+              <span>Переглянути детальнішу інформацію</span>
+            </span>
+          </div>
+        </div>
+        <button className="userprofile-logoutbutton">
+          <span className="userprofile-text136">
+            <span>Вийти з облікового запису</span>
+          </span>
+        </button>
+        <div className="userprofile-component37">
+          <span className="userprofile-text138">
+            <span>Видалити обліковий запис</span>
+          </span>
+        </div>
+        <div className="userprofile-group37">
+          <div className="userprofile-group34">
+            <img
+              src="/external/logo9281-64cp-200h.png"
+              alt="logo9281"
+              className="userprofile-logo"
+            />
+            <div className="userprofile-categories">
+              <button className="userprofile-sectionsbutton">
+                <span className="userprofile-text140">
+                  <span>Фільми</span>
+                </span>
+              </button>
+              <div className="userprofile-component281">
+                <span className="userprofile-text142">
+                  <span>Аніме</span>
+                </span>
+              </div>
+              <div className="userprofile-component271">
+                <span className="userprofile-text144">
+                  <span>Серіали</span>
+                </span>
+              </div>
+              <div className="userprofile-component291">
+                <span className="userprofile-text146">
+                  <span>Мультфільми</span>
+                </span>
+              </div>
+            </div>
+            <div className="userprofile-language">
+              <div className="userprofile-uaeng">
+                <span className="userprofile-text148">
+                  <span>UA</span>
+                </span>
+              </div>
+              <div className="userprofile-uaeng1">
+                <span className="userprofile-text150">
+                  <span>ENG</span>
+                </span>
+              </div>
+              <img
+                src="/external/line29281-k0im.svg"
+                alt="Line29281"
+                className="userprofile-line2"
+              />
+            </div>
+            <img
+              src="/external/searchbutton9281-cxr6.svg"
+              alt="searchbutton9281"
+              className="userprofile-searchbutton"
+            />
+            <button className="userprofile-profilebutton">
+              <div className="userprofile-group">
+                <img
+                  src="/external/vectori928-2rg9.svg"
+                  alt="VectorI928"
+                  className="userprofile-vector2"
+                />
+                <img
+                  src="/external/vectori928-fcv.svg"
+                  alt="VectorI928"
+                  className="userprofile-vector3"
+                />
+              </div>
+            </button>
+            <img
+              src="/external/component419281-w1j4.svg"
+              alt="Component419281"
+              className="userprofile-component41"
+            />
+            <img
+              src="/external/component429281-rb5e.svg"
+              alt="Component429281"
+              className="userprofile-component42"
+            />
+          </div>
+          <div className="userprofile-adresa11">
+            <span className="userprofile-text152">
+              <span>Введіть назву</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Userprofile
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import ReactDOM from 'react-dom/client'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
